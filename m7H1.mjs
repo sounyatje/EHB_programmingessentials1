1167cccd208be44caab6032e43958e0fcfb75488
sounya94 <sounya-abattouy@live.be>
2025-12-11 21:55:33 +0100

    commit

--- m7H1.mjs
+++ m7H1.mjs
@@ -2,3 +2,12 @@
 // Creëer een object genaamd autoGarage met eigenschappen naam (string) en autos (array van objecten, 
 //     waar elk object een auto vertegenwoordigt met eigenschappen zoals merk en bouwjaar). 
 // Voeg een methode vervangAuto toe waarmee je een oude auto kunt vervangen door een nieuwe.
+
+autoGarage = {naam:"autoservice", autos :[{merk:"bmw", bouwjaar:2015},{merk:"volkswagen", bouwjaar:2020},{merk:"suzuki",bouwjaar:2019}]}
+
+
+function vervangAuto (){
+    autoGarage.autos[index] = nieuweAuto;
+}
+vervangAuto(0, { merk: "audi", bouwjaar: 2022 });
+
