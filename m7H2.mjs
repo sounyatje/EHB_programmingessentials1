d19f801df8f81f1223f524384937a75c74e8da30
sounya94 <sounya-abattouy@live.be>
2025-12-11 22:02:49 +0100

    commit

--- m7H2.mjs
+++ m7H2.mjs
@@ -1,3 +1,32 @@
 // H2: Rekenmachine (again)
 // Maak een object genaamd rekenmachine met methoden voor optellen, aftrekken, vermenigvuldigen en delen. 
 // Elk van deze methoden moet twee parameters accepteren en het resultaat teruggeven.
+
+let rekenmachine = {
+  optellen,
+  aftrekken,
+  vermenigvuldigen,
+  delen
+};
+
+
+function optellen(a, b) {
+  return a + b;
+}
+
+function aftrekken(a, b) {
+  return a - b;
+}
+
+function vermenigvuldigen(a, b) {
+  return a * b;
+}
+
+function delen(a, b) {
+  return a / b;
+}
+
+console.log(rekenmachine.optellen(2, 3));        
+console.log(rekenmachine.aftrekken(10, 4));      
+console.log(rekenmachine.vermenigvuldigen(3, 5)); 
+console.log(rekenmachine.delen(20, 4));          
