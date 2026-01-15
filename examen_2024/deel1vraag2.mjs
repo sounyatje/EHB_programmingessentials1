88097136edd63cc1c0b3247d18b68d175e2de995
sounya94 <sounya-abattouy@live.be>
2026-01-15 10:36:47 +0100

    commit

--- examen_2024/deel1vraag2.mjs
+++ examen_2024/deel1vraag2.mjs
@@ -26,23 +26,45 @@ Output:
 1234
 1234
 */
+// import * as readline from "node:readline/promises";
+// import { stdin as input, stdout as output } from "node:process";
+// const userInput = readline.createInterface({ input, output });
+
+// let hoogte = parseInt(await userInput.question("de hoogte: "));
+// let rij = await userInput.question("rij of kolom: ");
+
+// let verhoogrij = rij === "rij";
+
+// for (let i = 1; i <= hoogte; i++) {
+//     let lijn = "";                        //wat doet rij en wat doet lijn? verschil
+//     for (let j = 1; j <= hoogte; j++) {
+//         if (verhoogrij) {
+//             lijn = lijn + i; //verhoog i
+//         } else {
+//             lijn = lijn + j; // verhoog j
+//         }
+//     }
+//     console.log(lijn);
+// }
+
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 const userInput = readline.createInterface({ input, output });
 
 let hoogte = parseInt(await userInput.question("de hoogte: "));
-let rij = await userInput.question("rij of kolom: ");
-
-let verhoogrij = rij === "rij";
+let rk = await userInput.question("rij of kolom: ");
 
 for (let i = 1; i <= hoogte; i++) {
-    let lijn = "";                        //wat doet rij en wat doet lijn? verschil
+  let lijn = "";
+  if (rk === "rij") {
     for (let j = 1; j <= hoogte; j++) {
-        if (verhoogrij) {
-            lijn = lijn + i; //verhoog i
-        } else {
-            lijn = lijn + j; // verhoog j
-        }
+      lijn = lijn + i;
+    }
+     console.log(lijn);
+  } else {
+    for (let j = 1; j <= hoogte; j++) {
+      lijn = lijn + j;
     }
     console.log(lijn);
+  }
 }
--- pe_sounya_abattouy/1234.mjs
+++ pe_sounya_abattouy/1234.mjs
@@ -1,12 +1,32 @@
-//1 2 3
-//3 6 9
-//5 10 15
+// /* Vraag 3: Functies/Arrays (20 punten) */
 
-for(let i=1; i<=5; i=i+2){
-  let lijn = ""
-  for(let j = 1; j<=3 ; j++){
-    lijn = lijn + (i+j) + " "
-     
-  }
- console.log(lijn)
-}
+// Schrijf de volgende functies die een array binnenkrijgen als parameter. 
+// Die array bevat een lijst van positieve getallen. De functies returnen een geupdatete lijst
+
+// voegPositiefGetalToe(lijst, getal): Voeg een getal toe aan het einde van de lijst. 
+// Zorg dat de functie het getal enkel toevoegt als het ingegeven getal hoger is dan 0
+// maakLijstLeeg(lijst): Maak de array leeg zodat deze niets meer bevat.
+// toonAantalEven(lijst): Deze functie toont het aantal even getallen (geheel deelbaar door 2) die zich in de lijst bevinden. 
+// Toon dat de lijst leeg is indien er helemaal geen getallen zijn
+
+// Zorg ervoor dat de output overeenkomt met het onderstaande voorbeeld, de functies moet je op deze manier kunnen oproepen:
+
+ //let lijst = []
+// lijst = voegPositiefGetalToe(lijst, 6);
+// lijst = voegPositiefGetalToe(lijst, -5);
+ // Het getal -5 mag niet toegevoegd zijn.
+// lijst = voegPositiefGetalToe(lijst, 9);
+// lijst = voegPositiefGetalToe(lijst, 250);
+// lijst = voegPositiefGetalToe(lijst, 16);
+
+// toonAantalEven(lijst);
+
+ // Het resultaat hiervan moet op dit moment zijn:
+//Het aantal even getallen: 3
+
+// lijst = maakLijstLeeg(lijst);
+// toonAantalEven(lijst);
+
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+const userInput = readline.createInterface({ input, output });
